0b9c17dd5d736025ea662ed32deec7c197a1a341
imomoh <imomoh96@gmail.com>
2020-11-29 21:47:45 -0500

    Initial Commit

---
+++ Naijahacks2020IOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Naijahacks2020IOS
+//
+//  Created by Ibrahim Momoh on 11/29/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
