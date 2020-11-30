f8bca6cd302e5871d153aba759271ac0b41d254c
imomoh <imomoh96@gmail.com>
2020-11-29 23:54:22 -0500

    all image Asset transferred and Login UIComplete

--- Naijahacks2020IOS/AppDelegate.swift
+++ Naijahacks2020IOS/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let configuration = ParseClientConfiguration {
+            $0.applicationId = "mJsouPJeB5MoosPBcstSfAVZso9j5FF8248w3AgE"
+            $0.clientKey = "m2alaTyMRBb7oBrwtFQ9H7Fc6SOTS3bCqDl06uOD"
+            $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: configuration)
+        
+        
         return true
     }
 
